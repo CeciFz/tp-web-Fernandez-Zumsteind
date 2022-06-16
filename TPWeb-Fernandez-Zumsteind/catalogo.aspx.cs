d5e96268253ee51f82c7be5e56171272dfa56907
Ceci <ceci.fz87@gmail.com>
2022-06-15 23:44:53 -0300

    Fix pagina principal

---
+++ TPWeb-Fernandez-Zumsteind/catalogo.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
+
+namespace TPWeb_Fernandez_Zumsteind
+{
+    public partial class Articulos : System.Web.UI.Page
+    {
+        public List<Articulo> listaArticulos { get;set;}
+
+        
+        protected void Page_Load(object sender, EventArgs e)
+        { 
+            ArticuloNegocio articuloNegocio = new ArticuloNegocio();
+            listaArticulos = articuloNegocio.listarArticulos();           
+
+        }
+
+        public void cargarImagen(string imagen)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(imagen))
+                {
+                    imgArticulo.ImageUrl = imagen;
+                }
+                else {
+                    imgArticulo.ImageUrl = "https://www.blackwallst.directory/images/NoImageAvailable.png";
+                }
+            }
+            catch (Exception ex)
+            {
+                imgArticulo.ImageUrl = "https://www.blackwallst.directory/images/NoImageAvailable.png";
+            }
+        }
+            
+
+    }
+}
